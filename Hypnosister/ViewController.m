b2d75cd213ca980164f6c4a4ce6e66ae66077ce8
Joenabie Gamao <joenabie@me.com>
2017-02-05 23:03:40 +0800

    Initial Commit

---
+++ Hypnosister/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Hypnosister
+//
+//  Created by Abz Maxey on 05/02/2017.
+//  Copyright © 2017 Abz Maxey. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ Hypnosister/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Hypnosister
+//
+//  Created by Abz Maxey on 05/02/2017.
+//  Copyright © 2017 Abz Maxey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
